413fa261e0c2413d4bf736b699e4c6a40c9bc9cf
bvk2412001 <khoakl241@gmail.com>
2022-12-21 16:04:21 +0700

    khoa

---
+++ assets/script/BirdController.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('BirdController')
+export class BirdController extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
